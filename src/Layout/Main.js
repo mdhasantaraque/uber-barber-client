ef0cd24bd23ab9089434b52e30c22d33365bd065
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-11-08 14:06:04 +0600

    header added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Share/Footer";
+import Header from "../Share/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
 import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
